0100df6c614cebfcf8be419e9c38c00da19fd13d
Nelson Flores <nelson.flores@live.com>
2024-01-05 17:54:48 +0200

    updates

--- resources/views/main/templates/elements/header.blade.php
+++ resources/views/main/templates/elements/header.blade.php
@@ -8,9 +8,30 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
-          <li><a class="nav-link scrollto" href="#about">Cursos</a></li>
-          <li class="dropdown"><a href="#"><span>Usuarios</span> <i class="bi bi-chevron-down"></i></a>
+
+
+            @foreach ($site_menu??[] as $item)
+
+            @if ($item->prefer == "uri")
+
+            @php
+                $link = url($item->uri);
+            @endphp
+
+            @else
+
+            @php
+                $link = route($item->uri);
+            @endphp
+
+            @endif
+
+
+
+            <li><a class="nav-link" href="{{$link}}">{{ $item->name }}</a></li>
+            @endforeach
+
+          <!--li class="dropdown"><a href="#"><span>Usuarios</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
@@ -27,7 +48,7 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li-->
           <li><a class="getstarted scrollto" href="#about">Whatsapp</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
