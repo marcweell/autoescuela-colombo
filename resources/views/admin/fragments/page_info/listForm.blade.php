85d22b51e0385571924753fdad36b60c10ae05f7
Nelson Flores <nelson.flores@live.com>
2024-01-06 11:30:34 +0200

    updates

--- resources/views/admin/fragments/page_info/listForm.blade.php
+++ resources/views/admin/fragments/page_info/listForm.blade.php
@@ -28,7 +28,7 @@
                                      <small class="mb-0 font-10">
                                          @if (empty($item->content))
                                              {{ '[' . __('No definido') . ']' }}
-                                         @elseif(($item->content_type == 'plain_text' or $item->content_type == 'number') and strlen($item->content) < 20)
+                                         @elseif(in_array($item->content_type,['plain_text', 'color', 'number', 'date', 'time']) and strlen($item->content) < 20)
                                          {{ $item->content }}
                                          @else
                                          {{ '[' . __('Ver detalles') . ']' }}
