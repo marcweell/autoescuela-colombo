960f942054153569d5dd9f69cec9e2ab21d96bc6
Nelson Flores <nelson.flores@live.com>
2024-01-08 02:30:33 +0200

    updates

--- resources/views/main/templates/elements/sidebar.blade.php
+++ resources/views/main/templates/elements/sidebar.blade.php
@@ -1,8 +1,8 @@
       <!-- Sidebar menu-->
       <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
       <aside class="app-sidebar">
-          <div class="app-sidebar__user"><img class="app-sidebar__user-avatar nf_picture"
-                  src="{{ tools()->photo($user->photo) }}" alt="User Image">
+          <div class="app-sidebar__user">
+              <img class="app-sidebar__user-avatar nf_picture" src="{{ tools()->photo($user->photo) }}" alt="User Image" />
               <div>
                   <p class="app-sidebar__user-name">{{ $user->name . ' ' . $user->last_name }}</p>
                   <p class="app-sidebar__user-designation">{{ $user->email }}</p>
@@ -10,46 +10,35 @@
           </div>
           <ul class="app-menu">
 
-
-
-              <li class="treeview"><a class="app-menu__item" href="javascript:void()" data-toggle="treeview"><i
-                          class="app-menu__icon fa fa-newspaper"></i><span class="app-menu__label">Examen</span><i
-                          class="treeview-indicator fa fa-chevron-right"></i></a>
+              <li class="treeview">
+                  <a class="app-menu__item" href="javascript:void()" data-toggle="treeview">
+                      <i class="app-menu__icon fa fa-newspaper"></i><span class="app-menu__label">Examen</span>
+                      <i class="treeview-indicator fa fa-chevron-right"></i>
+                  </a>
                   <ul class="treeview-menu">
-                      <li><a class="treeview-item _link_" data-href="{{ route('web.app.survey.survey.index') }}"
-                              href="javascript:void()"><i class="icon fa fa-circle"></i> Exámenes disponibles</a></li>
+                      <li><a class="treeview-item _link_" data-href="{{ route('web.app.survey.survey.index') }}" href="javascript:void()"><i class="icon fa fa-circle"></i> Exámenes Disponibles</a></li>
                       <hr class="my-1">
-                      <li><a class="treeview-item _link_"
-                              data-href="{{ route('web.app.survey.survey_answer.index') }}"
-                              href="javascript:void()"><i class="icon fa fa-circle"></i>Mi Respuestas</a></li>
+                      <li><a class="treeview-item _link_" data-href="{{ route('web.app.survey.survey_answer.index') }}" href="javascript:void()"><i class="icon fa fa-circle"></i>Mi Respuestas</a></li>
                   </ul>
               </li>
-
-
-
               <!-- ----------- -->
 
 
               <li>
-                  <a class="app-menu__item _link_" data-href="{{ route('web.app.profile.index') }}"
-                      href="javascript:void()" target="_blank">
-                      <i class="app-menu__icon fa fa-cog"></i>
+                  <a class="app-menu__item _link_" data-href="{{ route('web.app.profile.index') }}" href="javascript:void()" target="_blank">
+                      <i class="app-menu__icon fa fa-user-cog"></i>
                       <span class="app-menu__label">Cuenta</span>
                   </a>
               </li>
-
               <!-- ----------- -->
 
 
               <li>
-                  <a class="app-menu__item _link_ prompt" data-href="{{ route('web.account.auth.logout') }}"
-                      href="javascript:void()" target="_blank">
-                      <i class="app-menu__icon fa fa-door-open"></i>
-                      <span class="app-menu__label"> Salir</span>
+                  <a class="app-menu__item _link_ prompt" data-href="{{ route('web.account.auth.logout') }}" href="javascript:void()" target="_blank">
+                      <i class="app-menu__icon fa fa-sign-out"></i>
+                      <span class="app-menu__label"> Cerrar sesión</span>
                   </a>
               </li>
-
-
               <!-- ----------- -->
 
 
