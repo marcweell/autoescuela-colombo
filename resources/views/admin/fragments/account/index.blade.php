a7370c3d7a36b039a5a5ab1be314dcbebaa75454
Nelson Flores <nelson.flores@live.com>
2024-01-09 03:53:56 +0200

    updates

--- resources/views/admin/fragments/account/index.blade.php
+++ resources/views/admin/fragments/account/index.blade.php
@@ -69,6 +69,15 @@
                         <hr>
                         <div class="row">
                             <div class="col-sm-3">
+                                <p class="mb-0">Correo</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0">{{ $user->email }}</p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
                                 <p class="mb-0">Direccion</p>
                             </div>
                             <div class="col-sm-9">
